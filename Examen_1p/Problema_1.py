6c47755cd4f0409698a8bc920ead945571afcee0
Mane-ops <122042767@upq.edu.mx>
2024-02-07 19:33:20 -0600

    Agregué comentarios a codigos

--- Examen_1p/Problema_1.py
+++ Examen_1p/Problema_1.py
@@ -1,3 +1,4 @@
+#Programa que solicita una palabra y lo descompone en letras
 palabra = str(input('Ingresa una palabra: '))
 x=1
 for i in palabra:
--- Examen_1p/Problema_2.py
+++ Examen_1p/Problema_2.py
@@ -1,3 +1,4 @@
+#Programa que genera la serie de Collatz
 num = int(input('Introduce un numero entero: '))
 while num > 1 :
     if num%2 == 0:
